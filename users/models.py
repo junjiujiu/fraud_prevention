2735e53e75722dcb2a9f36f11f56e07bd64bf1fe
lemon <601749122@qq.com>
2022-03-30 00:49:07 +0800

    Make model standard

--- users/models.py
+++ users/models.py
@@ -1,35 +1,41 @@
 from django.contrib import admin
 from django.db import models
+from django.utils import timezone
 
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    openid = models.CharField('openid',max_length=255)
-    userName = models.CharField('用户名',max_length=255)
-    avatarUrl = models.CharField('头像',max_length=255)
+    openid = models.CharField('openid', max_length=255)
+    userName = models.CharField('用户名', max_length=255, null=True)
+    avatarUrl = models.CharField('头像', max_length=255, null=True)
+
 
 class News(models.Model):
     # 自增主键, 这里不能设置default属性，负责执行save的时候就不会新增而是修改元素
     id = models.AutoField(primary_key=True)
-    title = models.CharField("标题",max_length=255)
-    content = models.CharField("内容",max_length=255)
-    createTime = models.DateTimeField('创建时间', auto_now_add=True, editable=True, null=True)
-    source = models.CharField("发起人",max_length=255)
-    type = models.CharField("类型",default="1",choices=(('1',"近期新闻"),('2',"经典案例")),max_length=1)
-    img = models.CharField(max_length=255,null=True,blank=True)
+    title = models.CharField("标题", max_length=255)
+    content = models.TextField("内容")
+    createTime = models.DateTimeField('创建时间',  null=True, default=timezone.now)
+    source = models.CharField("发起人", max_length=255)
+    type = models.CharField("类型", default="1", choices=(('1', "近期新闻"), ('2', "经典案例")), max_length=1)
+    img = models.CharField(max_length=255, null=True, blank=True)
 
-class assessMessage(models.Model):
+
+class AssessMessage(models.Model):
     id = models.AutoField(primary_key=True)
     userid = models.IntegerField()
-    messageInfo = models.CharField("短信内容",max_length=500)
+    messageInfo = models.TextField("短信内容")
     percentage = models.FloatField("是诈骗信息的概率")
 
-class redis(models.Model):
+
+class Redis(models.Model):
     id = models.AutoField(primary_key=True)
     mkey = models.CharField("标题", max_length=50)
     content = models.CharField("内容", max_length=500)
     expireTime = models.DateTimeField("过期时间")
 
 
+# 添加后台django的CRUD界面
+admin.site.register(User)
 admin.site.register(News)
-admin.site.register(assessMessage)
+admin.site.register(AssessMessage)
