32ff96367ae6f781160f2b8f27f69ec76ecf06dc
lemon <601749122@qq.com>
2022-03-31 00:39:34 +0800

    Set swindledNum nullable in model.User and make USE_TZ False in settings.py

--- applet/models.py
+++ applet/models.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
-
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     openid = models.CharField('openid', max_length=255)
     userName = models.CharField('用户名', max_length=255, null=True)
     avatarUrl = models.CharField('头像', max_length=255, null=True)
-    swindledNum = models.FloatField('被诈骗指数', default=100)
+    swindledNum = models.FloatField('被诈骗指数', null=True)
 
 
 class News(models.Model):
